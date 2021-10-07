e9862c6fd7e36c62ae769efea667d1c0371b735d
Clay Risser <clayrisser@gmail.com>
2021-10-06 22:22:45 -0700

    Improved bootstrap

--- mkpm.mk
+++ mkpm.mk
@@ -29,14 +29,16 @@ MKPM_REPOS := \
 ############# MKPM BOOTSTRAP SCRIPT BEGIN #############
 MKPM_BOOTSTRAP := https://bitspur.gitlab.io/community/mkpm/bootstrap.mk
 NULL := /dev/null
+TRUE := true
 ifeq ($(OS),Windows_NT)
+	.SHELLFLAGS = /q /v /c
 	NULL = nul
 	SHELL = cmd.exe
-	.SHELLFLAGS = /q /v /c
+	TRUE = type nul
 endif
 -include .mkpm/.bootstrap.mk
 .mkpm/.bootstrap.mk:
-	@mkdir .mkpm
+	@mkdir .mkpm 2>$(NULL) || $(TRUE)
 	@cd .mkpm && \
 		$(shell curl --version >$(NULL) 2>$(NULL) && \
 			echo curl -L -o || \
