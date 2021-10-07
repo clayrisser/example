e7eb5b9926f62072aaa746af3cf7991702e2424f
Clay Risser <clayrisser@gmail.com>
2021-10-07 16:46:16 -0500

    Updated example mkpm

--- mkpm.mk
+++ mkpm.mk
@@ -3,7 +3,7 @@
 # File Created: 27-09-2021 19:45:54
 # Author: Clay Risser
 # -----
-# Last Modified: 03-10-2021 18:24:39
+# Last Modified: 07-10-2021 16:46:09
 # Modified By: Clay Risser
 # -----
 # BitSpur Inc (c) Copyright 2021
@@ -29,14 +29,14 @@ MKPM_REPOS := \
 ############# MKPM BOOTSTRAP SCRIPT BEGIN #############
 MKPM_BOOTSTRAP := https://bitspur.gitlab.io/community/mkpm/bootstrap.mk
 NULL := /dev/null
+TRUE := true
 ifeq ($(OS),Windows_NT)
 	NULL = nul
-	SHELL = cmd.exe
-	.SHELLFLAGS = /q /v /c
+	TRUE = type nul
 endif
 -include .mkpm/.bootstrap.mk
 .mkpm/.bootstrap.mk:
-	@mkdir .mkpm
+	@mkdir .mkpm 2>$(NULL) || $(TRUE)
 	@cd .mkpm && \
 		$(shell curl --version >$(NULL) 2>$(NULL) && \
 			echo curl -L -o || \
